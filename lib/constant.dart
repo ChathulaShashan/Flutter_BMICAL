6abd095787a4fe9cf1deaa6d21aff03f6e5d98fb
ChathulaShashan <97799771+ChathulaShashan@users.noreply.github.com>
2023-06-30 17:04:49 +0530

    set some const ....

--- lib/constant.dart
+++ lib/constant.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
 
 const ktextColour =
-    Color(0xFFFF8888);       // normally constant start with a K ( it's a standard)
+    Color(0xFFFF8888); // normally constant start with a K ( it's a standard)
+const kinputLableColour = TextStyle(
+  color: ktextColour,
+  fontSize: 50,
+  fontWeight: FontWeight.bold,
+);
