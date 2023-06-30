77a5a26121c1440ad49f91f473323bc83b478df9
ChathulaShashan <97799771+ChathulaShashan@users.noreply.github.com>
2023-06-30 12:56:40 +0530

    up

--- lib/constant.dart
+++ lib/constant.dart
@@ -1,3 +1,4 @@
 import 'package:flutter/material.dart';
 
-const KtextColour = Color(0xFFFF8888)         // normally constant start with a K ( it's a standard)
+const ktextColour =
+    Color(0xFFFF8888);       // normally constant start with a K ( it's a standard)
